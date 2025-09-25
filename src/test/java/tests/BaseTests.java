7db67dc04dbd25769fffe71656b1081f85af1b14
QA academy <education@qaacademy.rs>
2025-09-25 19:11:39 +0200

    Firefox Web browser

--- src/test/java/tests/BaseTests.java
+++ src/test/java/tests/BaseTests.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import pages.BasePage;
 
 import java.time.Duration;
@@ -17,7 +18,7 @@ public class BaseTests {
 
     @BeforeClass
     public static void beforeClass() throws Exception {
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 //        max time to wait for a page load
